6be125276a5ef2fadcf172ccf4b865f3e09b2f33
Jason <acubed75@gmail.com>
2019-07-21 14:36:01 -0700

    added messages

--- src/game.js
+++ src/game.js
@@ -11,6 +11,8 @@ class Game {
 		this.dim_y = dim_y;
 		this.link;
 		this.countToThirty = 0;
+		this.messageCount = 0;
+		this.message = "";
 
 		this.add(new Enemy({pos: [50, 30],
 	 											vel: [1, 1],
@@ -31,19 +33,39 @@ class Game {
 	}
 
 	draw(ctx) {
+	
 
 		ctx.clearRect(0,0,this.dim_x, this.dim_y);
 		
+		this.drawMessage(ctx);
+
+
 		this.enemies.forEach(ele => { ele.drawObject(ctx); });
 		
-		if (this.countToThirty > 0 && this.countToThirty%3 === 0) 
+		if (this.countToThirty > 0 && this.countToThirty%2 === 0 && this.collision) 
 			this.link.drawObject(ctx, true );
 		else this.link.drawObject(ctx, false);
 	}
 
+	drawMessage(ctx){
+		if (this.messageCount === 40) this.messageCount = 0;
+		else if (this.messageCount > 0) {
+
+			ctx.font = "20px Comic Sans MS";
+			ctx.fillStyle = "black";
+			ctx.textAlign = "center";
+			ctx.fillText(this.message, this.dim_x / 2, 20);
+			this.messageCount++;
+		}
+	}
+
+
 	step() {
 		// debugger
-		if (this.countToThirty === 30) this.countToThirty = 0;
+		if (this.countToThirty === 30) {
+			this.countToThirty = 0;
+			this.collision = false;
+		}
 		else if (this.countToThirty > 0) this.countToThirty++;
 		else {
 			this.checkCollisions();
@@ -58,13 +80,16 @@ class Game {
 		const linkCenter = this.link.center();
 
 		const rad = this.link.radius;
+		this.collision = false;
 
 		this.enemies.forEach(enemy => { 
 			const distance = Util.distance(linkCenter,enemy.pos);
 
 			if (distance < (rad + enemy.radius +2)) {
 				this.countToThirty++;
-				console.log("collision");
+				this.collision = true;
+				this.message = "Ouch!";
+				this.messageCount++;
 			}
 		})
 	}
@@ -76,8 +101,9 @@ class Game {
 				const distance = Util.distance(tip, enemy.pos);
 
 				if (distance < enemy.radius) {
-					this.countToThirty++;					
-					console.log("hit");
+					this.countToThirty++;		
+					this.message = "Hit!"	
+					this.messageCount++;		
 				} 
 			})
 	 	}
